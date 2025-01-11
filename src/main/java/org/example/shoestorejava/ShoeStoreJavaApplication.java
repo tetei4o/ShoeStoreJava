61b5d7463df020c2c929d236bf19d5fa705d9b2a
Atanas Yurukov <atanas.yurukov1407@gmail.com>
2025-01-11 14:52:04 +0200

    Initial project files commit

---
+++ src/main/java/org/example/shoestorejava/ShoeStoreJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.shoestorejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeStoreJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeStoreJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/shoestorejava/ShoeStoreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shoestorejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeStoreJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
